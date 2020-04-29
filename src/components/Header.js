32a028eaaebc0328dfc89627402b87df3c1f6d30
sakinfens <sakinfens@gmail.com>
2020-04-28 21:44:34 -0400

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,18 @@ class Header extends React.Component{
   render(){
     return(
       <div>
-      	<nav>
-      		<h1>Steve Akinfenwa <br />Brain Game</h1>
+
+      	<div className="instructions">
+      		<div>
+      			<h2>Easy Meals<br /> With Forgetable Instructions</h2>
+      			<p>Instructions <br /> Welcome to one of many games I have created. The purpose of the brain games is to get people thinking in ways they don't normally think. In this particular game you will have foods that are super simple but the objective is to give the most elaborate instructions on how to make this food. You can either add on to a meal or you can start your own. Let's get started!</p>
+      		</div>
+      		<div>
+            <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/grilled-cheese-horizontal-jpg-1522266016.jpg"></img>
+      		</div>
+      	</div>
+        <nav>
+          <h1>Steve Akinfenwa <br />Brain Game</h1>
           <section>
             <h1>NAVIGATE</h1>
             <div>
@@ -17,16 +27,8 @@ class Header extends React.Component{
               }}>add meal</p>
             </div>
           </section>
-      	</nav>
-      	<div className="instructions">
-      		<div>
-      			<h2>Easy Meals<br /> With Forgetable Instructions</h2>
-      			<p>Instructions <br /> Welcome to one of many games I have created. The purpose of the brain games is to get people thinking in ways they don't normally think. In this particular game you will have foods that are super simple but the objective is to give the most elaborate instructions on how to make this food. You can either add on to a meal or you can start your own. Let's get started!</p>
-      		</div>
-      		<div>
-            <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/grilled-cheese-horizontal-jpg-1522266016.jpg"></img>
-      		</div>
-      	</div>
+        </nav>
+
       </div>
     )
   }
